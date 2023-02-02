099bc3f668e030e9e428a7f3c029169f9744f705
maxguy2001 <max.guy2001@gmmail.com>
2023-02-02 16:26:43 +0000

    logistic regression initial implementation

--- Part1/functions.py
+++ Part1/functions.py
@@ -1,6 +1,8 @@
 import numpy as np
 # from sklearn.x import y
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold
 
 
 def tidyData(df):
@@ -15,7 +17,34 @@ def evaulateModel():
 
 
 def fitLogisticRegression(df):
-    pass
+    # Separating features and target
+    y = df["SalePrice"]
+    X = df.drop(["SalePrice"], axis=1, inplace=True)
+
+    # Make cross validation generator
+    cv_generator = KFold(n_splits=10, shuffle=True, random_state=3)
+
+    # initialise list for scores
+    scores = []
+
+    # make and scoe model on each fold
+    for train_index, test_index in cv_generator.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+
+        #fit model
+        clf = LogisticRegression(random_state=3).fit(X_train, y_train)
+
+        #make prediction
+        y_pred = clf.predict_proba(X_test)
+
+        #score predition
+        score = evaulateModel(y_test, y_pred)
+
+        #add score to list
+        scores.append(score)
+
+    return scores
 
 
 def fitNaiveBayes(df):
