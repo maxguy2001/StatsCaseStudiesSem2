1c770ac6ce8f3c89ded5d13ca3ae9cba1e90812f
maxguy2001 <max.guy2001@gmmail.com>
2023-02-02 15:11:39 +0000

    skeletion functions

---
+++ Part1/functions.py
@@ -0,0 +1,16 @@
+import numpy as np
+# from sklearn.x import y
+import pandas as pd
+
+
+def tidyData(df):
+    # TODO tidy the data
+    return df
+
+
+def fitLogisticRegression(df):
+    pass
+
+
+def fitNaiveBayes(df):
+    pass
