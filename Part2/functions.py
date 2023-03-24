480a9482954500263af38bdacdd1230277b46b4f
maxguy2001 <max.guy2001@gmmail.com>
2023-03-24 17:32:38 +0000

    added full functionality. Questions still  need fixed and run

--- Part2/functions.py
+++ Part2/functions.py
@@ -4,7 +4,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import LeaveOneOut
-from multiprocessing import Queue
+from sklearn.ensemble import RandomForestRegressor
+
+
+def getUseableHPData():
+    # read data
+    df = pd.read_csv("houseprices.csv")
+
+    # preproccessing
+    garage_types = df["GarageType"].tolist()
+    fixed_garage_types = []
+
+    for i in garage_types:
+        if type(i) != str:
+            fixed_garage_types.append("NoGarage")
+        else:
+            fixed_garage_types.append(i)
+
+    df["GarageType"] = fixed_garage_types
+
+    df.dropna(inplace=True)
+    df = pd.get_dummies(df)
+    return df
+
+
+def getUseableHP2Data():
+    # read data
+    df = pd.read_csv("houseprices2.csv")
+
+    # preproccessing
+    garage_types = df["GarageType"].tolist()
+    fixed_garage_types = []
+
+    for i in garage_types:
+        if type(i) != str:
+            fixed_garage_types.append("NoGarage")
+        else:
+            fixed_garage_types.append(i)
+
+    df["GarageType"] = fixed_garage_types
+
+    df.dropna(inplace=True)
+    df = pd.get_dummies(df)
+    return df
+
 
 def trainLinearRegression(X, y):
     """
@@ -20,24 +63,23 @@ def trainLinearRegression(X, y):
     reg = LinearRegression()
 
     for train_index, test_index in loo_generator.split(X):
-        #subset dataframe
+        # subset dataframe
         X_train, X_test = X.iloc[train_index, :], X.iloc[test_index, :]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-        #fit model
+        # fit model
         reg.fit(X_train, y_train)
 
-        #predict missing datapoint
+        # predict missing datapoint
         y_pred = reg.predict(X_test)
 
-        #add absolute difference to list
+        # add absolute difference to list
         abs_errors.append(abs(y_test.to_numpy() - y_pred)[0])
 
     return np.sum(abs_errors)/len(abs_errors)
 
 
-
-def quickScoreLinearRegression(colnames):
+def quickScoreLinearRegression(colnames, use_hp_2=False):
     """
     arguments_list contains a list of function arguments with the following:
     - queue: multiprocessing queue
@@ -47,50 +89,80 @@ def quickScoreLinearRegression(colnames):
 
     This funciton is built in this way to optimize for parallel processing of results.
     """
-    #read and preprocess data
-    df = pd.read_csv("houseprices.csv") 
-    garage_types = df["GarageType"].tolist()
-    fixed_garage_types = []
-
-    for i in garage_types:
-        if type(i) != str:
-            fixed_garage_types.append("NoGarage")
-        else:
-            fixed_garage_types.append(i)
-
-    df["GarageType"] = fixed_garage_types
-
-    df.dropna(inplace=True)
-    df = pd.get_dummies(df)
+    # read and preprocess data
+    if use_hp_2 == False:
+        df = getUseableHPData()
+    else:
+        df = getUseableHP2Data()
 
     y = df["SalePrice"]
     X = df[colnames]
-  
+
     abs_errors = []
 
     loo_generator = LeaveOneOut()
     reg = LinearRegression()
 
     for train_index, test_index in loo_generator.split(X):
-        #subset dataframe
+        # subset dataframe
         if len(colnames) == 1:
             X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         else:
             X_train, X_test = X.iloc[train_index, :], X.iloc[test_index, :]
-            
+
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-        #fit model
+        # fit model
         reg.fit(X_train, y_train)
 
-        #predict missing datapoint
+        # predict missing datapoint
         y_pred = reg.predict(X_test)
 
-        #add absolute difference to list
+        # add absolute difference to list
         abs_errors.append(abs(y_test.to_numpy() - y_pred)[0])
 
     mae = np.sum(abs_errors)/len(abs_errors)
     return mae
-#print(f"Column {X.columns} had score, {mae}.")
-    
-    
+
+
+def quickScoreRandomForest():
+    """
+    arguments_list contains a list of function arguments with the following:
+    - queue: multiprocessing queue
+    - y: the column vector we wish to predict
+    - X: the pandas dataframe of predictors
+    we wish to use in our model
+
+    This funciton is built in this way to optimize for parallel processing of results.
+    """
+    # read and preprocess data
+    # TODO: convert back to useable hp 2!
+    df = getUseableHPData()
+
+    y = df["SalePrice"]
+    X = df.copy(deep=True)
+    X.drop(["SalePrice"], axis=1, inplace=True)
+
+    abs_errors = []
+
+    # TODO:change to 10-fold cv
+    loo_generator = LeaveOneOut()
+    rf = RandomForestRegressor()
+
+    for train_index, test_index in loo_generator.split(X):
+        # subset dataframe
+        X_train, X_test = X.iloc[train_index, :], X.iloc[test_index, :]
+
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+
+        # fit model
+        rf.fit(X_train, y_train)
+
+        # predict missing datapoint
+        y_pred = rf.predict(X_test)
+
+        # add absolute difference to list
+        abs_errors.append(abs(y_test.to_numpy() - y_pred)[0])
+
+    mae = np.sum(abs_errors)/len(abs_errors)
+    return mae
